558703f0a8938165667412c502a2c83220e53424
Course Brains <99380906+Course-Brains@users.noreply.github.com>
2026-08-19 11:57:47 +0100

    Added spell info and designed health better
    Made it so that spell will give you information to deobfuscate casting
    and I made it so that enemy health is no longer measured in hits so that
    I don't have to constantly convert between enemy health and player
    health

--- src/spell/fireball.rs
+++ src/spell/fireball.rs
@@ -1,27 +1,42 @@
-// Base mana cost will be 5
-// base radius will be 3
-// base damage will be 10-41
-// base speed will be 20
-//
-// radius should increase with root of energy
-// damage should increase with energy
-// speed should decrease with energy settling at 5 in a reciprocal manner
-
 use super::*;
 use crate::board::Projectile;
 use crate::board::ProjectileType;
 use crate::random::Random;
+
 pub fn cast(state: &mut State, origin: Vector<usize>, target: Vector<usize>, energy_used: usize) {
-    let radius = (energy_used as f64).sqrt().ceil() as usize;
-    let damage_offset = energy_used * 2;
-    let damage_mult = energy_used / 5;
-    let damage = damage_offset + ((u8::random() & 32) as usize) * damage_mult;
-    let speed = (20.0 - 5.0 * energy_used as f64).max(5.0).ceil() as usize;
+    let damage =
+        damage_offset(energy_used) + ((u8::random() & 32) as usize) * damage_mult(energy_used);
     state.board.add_projectile(Projectile::new(
-        ProjectileType::Fireball { radius, damage },
-        speed,
+        ProjectileType::Fireball {
+            radius: radius(energy_used),
+            damage,
+        },
+        speed(energy_used),
         origin,
         target,
         true,
     ))
+}
+pub fn get_info(mana: usize) -> Vec<String> {
+    vec![
+        format!(
+            "Damage range: {}-{}",
+            damage_offset(mana),
+            damage_offset(mana) + 32 * damage_mult(mana)
+        ),
+        format!("Radius: {}", radius(mana)),
+        format!("Proj. speed: {}", speed(mana)),
+    ]
+}
+fn radius(mana: usize) -> usize {
+    (mana as f64).sqrt().ceil() as usize
+}
+fn damage_offset(mana: usize) -> usize {
+    mana * 2
+}
+fn damage_mult(mana: usize) -> usize {
+    mana / 5
+}
+fn speed(mana: usize) -> usize {
+    (21.0 - (mana / 3) as f64).max(5.0).ceil() as usize
 }
